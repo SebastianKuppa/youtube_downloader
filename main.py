2010ce4eeb20c7d92639aea0865a9bd4e2d9149a
test <sebastian.kuppa@web.de>
2022-12-12 15:18:51 +0800

    added downloader function, it gets the link provided in the entry and downloads the video

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from pytube import YouTube
 root = tkinter.Tk()
 root.title("Youtube Downloader")
 root.geometry("500x300")
-root.resizable(0, 0)
+root.resizable(False, False)
 
 tkinter.Label(root, text='Youtube Video Downloader', font='arial 20 bold').pack()
 
 link = tkinter.StringVar()
 tkinter.Label(root, text='Paste link here:', font='arial 15 bold').place(x=160, y=60)
 tkinter.Entry(root, width=70, textvariable=link).place(x=32, y=90)
+
+
+def downloader():
+    url = YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    tkinter.Label(root, text='DOWNLOADED', font='arial 15').place(x=180, y=210)
 
