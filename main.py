c194f812e1e598a8d1c0ce4190339cf0de1b659c
test <sebastian.kuppa@web.de>
2022-12-12 15:13:40 +0800

    added label for youtube link entry

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ root.title("Youtube Downloader")
 root.geometry("500x300")
 root.resizable(0, 0)
 
+tkinter.Label(root, text='Youtube Video Downloader', font='arial 20 bold').pack()
+
+link = tkinter.StringVar()
+tkinter.Label(root, text='Paste link here:', font='arial 15 bold').place(x=160, y=60)
+tkinter.Entry(root, width=70, textvariable=link).place(x=32, y=90)
+
